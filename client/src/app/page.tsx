e7c0ebaaf615a5fab0f989ac088ad684a4bef5a0
jackHotch <jhotchkiss2021@gmail.com>
2026-04-18 23:27:08 -0400

    created basic login page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,25 @@
 'use client'
 
-import { useUsers } from '@/hooks/use-users'
+import { Button } from '@/components/button'
+import { Clock } from 'lucide-react'
+import Image from 'next/image'
 
-export default function Home() {
-  const { data } = useUsers()
+export default function Login() {
   return (
-    <div>
-      {data?.map((user: { first_name: string; last_name: string; email: string }) => (
-        <div key={user.email}>{user.first_name} {user.last_name} — {user.email}</div>
-      ))}
+    <div className='flex h-screen w-screen'>
+      <div className='bg-card rounded-r-4xl w-[50vw] h-full p-8'>
+        <div className='flex items-center gap-2'>
+          <Clock color='var(--primary)' size={24} />
+          <h1 className='text-primary text-3xl font-bold'>Timesheet</h1>
+        </div>
+
+        <div>
+          <Button>Login</Button>
+        </div>
+      </div>
+      <div className='h-full w-[60vw]'>
+        <img src='/login-illustration.svg' alt='image' width='100%' height='100vh' />
+      </div>
     </div>
   )
 }
